1cfddf2816ee8860948b77bc08fce305afc2c543
/mahir Dhall <mahirdhall4@gmail.com>
2021-02-18 18:56:00 -0500

    Added legacyId to member mapping

--- tableColumnName.py
+++ tableColumnName.py
@@ -5,8 +5,8 @@ class TableColumnName:
         "col_name_insert": "challengeId, legacyId,directProjectId ,status, trackType, type , name , description , totalPrizeCost, winners,registrationStartDate , registrationEndDate, submissionStartDate , submissionEndDate , startDate , endDate, technologies, numOfSubmissions, numOfRegistrants, forumId"
     })
     Challenge_Member_Mapping = dict({
-        "col_name_create": "challengeId VARCHAR(50), memberHandle VARCHAR(50), submission Bool, winningPosition INT(255)",
-        "col_name_insert": "challengeId, memberHandle, submission, winningPosition"
+        "col_name_create": "challengeId VARCHAR(50),legacyId Int(255),memberHandle VARCHAR(50), submission Bool, winningPosition INT(255)",
+        "col_name_insert": "challengeId, legacyId,memberHandle, submission, winningPosition"
     })
     Members = dict({
         "col_name_create": "userId INT(255), memberHandle VARCHAR(512) PRIMARY KEY, DEVELOP Bool, DESIGN Bool, DATA_SCIENCE Bool, maxRating INT(255), track VARCHAR(512), subTrack VARCHAR(512), registrations INT(255), wins INT(255), user_entered MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL, participation_skill MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL",
